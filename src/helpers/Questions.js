0a1213031736b46a0a2791aeef6ecf440fa4322f
sakshamdev123 <sakshamdev9.com>
2023-09-09 23:38:49 +0530

    first commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "Which of the following is the correct identifier?",
+      optionA: "$var_name",
+      optionB: "VAR_123",
+      optionC: "varname@",
+      optionD: "None of the above",
+      answer: "optionB",
+    },
+    {
+      prompt: "Which of this is not a programming language?",
+      optionA: "Python",
+      optionB: "JavaScript",
+      optionC: "MC-03",
+      optionD: "Java",
+      answer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      answer: "optionD",
+    },
+  ];
